46e841858d37485307300b5e9626f1048ef7c09f
gnarmeister <sb9155118@naver.com>
2021-05-26 15:10:31 +0900

    move hash value interface

---
+++ HW5/code/HashValue.java
@@ -0,0 +1,3 @@
+interface HashValue<K> {
+    public K hashFunction();
+}
--- HW5/code/NewHashTable.java
+++ HW5/code/NewHashTable.java
@@ -3,7 +3,3 @@ import java.util.Hashtable;
 public class NewHashTable<K, V extends Comparable<V> & HashValue<K>> extends Hashtable<K, AVLTree<V>> {
 
 }
-
-interface HashValue<K> {
-    public K hashFunction();
-}

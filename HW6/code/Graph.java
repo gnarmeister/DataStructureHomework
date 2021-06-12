7a6f9c2121003be18c5df2e9ef4c393cfb39d990
gnarmeister <sb9155118@naver.com>
2021-06-12 12:54:01 +0900

    set basic structure

--- HW6/code/Graph.java
+++ HW6/code/Graph.java
@@ -1,33 +1,61 @@
+import java.util.ArrayList;
+
 public class Graph<K extends HashableKey, V> {
     public static int INF = -1;
     public int size;
-    public GraphNode<K, V>[] nodes;
+    public int secondSize;
+    public ArrayList<GraphNode<K, V>> nodes;
 
     Graph(GraphNode<K, V>[] inputNodes) {
-        size = findProperSize(inputNodes.length);
+        findProperSize(inputNodes.length);
         for (GraphNode<K, V> node : inputNodes) {
             add(node);
         }
     }
 
     public void add(GraphNode<K, V> node) {
-
+        int key = node.key.hashFunction(size);
+        int secondKey = node.key.hashFunction(secondSize);
+        while (true) {
+            if (nodes.get(key) == null) {
+                nodes.set(key, node);
+                return;
+            }
+            key += secondKey;
+        }
     }
 
     public GraphNode<K, V> get(K key) {
-        return nodes[0];
+        int firstKey = key.hashFunction(size);
+        int secondKey = key.hashFunction(secondSize);
+        int count = 0;
+        while (true) {
+            GraphNode<K, V> temp = nodes.get(firstKey);
+            count++;
+            if (temp != null && temp.key == key) {
+                return temp;
+            }
+            if (count >= size) {
+                // error handling required: TargetNotExists
+            }
+            firstKey += secondKey;
+        }
     }
 
-    private int findProperSize(int length) {
-//        int result = length * 2;
-//        while (true) {
-//            for (int i=2; i<=Math.sqrt(result); i++) {
-//                if (result % i == 0) {
-//                    break;
-//                }
-//            }
-//            result += 1;
-//        }
-        return 1;
+    private void findProperSize(int length) {
+        size = length*2;
+        for (int i=size; i>1; i--) {
+            if (isPrime(i)) {
+                secondSize = i;
+                return;
+            }
+        }
+    }
+
+    private boolean isPrime(int n) {
+        for (int i=2; i<=Math.sqrt(n); i++) {
+            if (n%i==0) return false;
+        }
+        return true;
     }
 }
--- HW6/code/GraphNode.java
+++ HW6/code/GraphNode.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
 
 public class GraphNode<K, V> {
+    public K key;
     public V value;
     public int distance = Graph.INF;
     public ArrayList<ArrayListElement<K>> adjacencyList = new ArrayList<>();
 
-    GraphNode(V v) {
+    GraphNode(K k, V v) {
+        key = k;
         value = v;
     }
 
